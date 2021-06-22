94a8fa5864f3c38919a20e73a9165567cec5f97d
Matthew M-B <matthewmacraebovell@outlook.com>
2021-06-22 01:12:21 -0400

    lottie cdn fix (#17)
    * win screen regardless of score
    
    * lint
    
    * lottie cdn fix

--- src/components/Confetti/Confetti.svelte
+++ src/components/Confetti/Confetti.svelte
@@ -7,7 +7,7 @@
   onMount(() => {
     let script = document.createElement('script');
     script.src =
-      'https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js';
+      'https://unpkg.com/@lottiefiles/lottie-player@0.4.0/dist/lottie-player.js';
     document.head.append(script);
   });
 </script>
